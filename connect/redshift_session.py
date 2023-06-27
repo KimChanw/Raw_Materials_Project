937fbe0f26e042a1c5ca05b5a322b7f9f493717a
chanwoo <chanwoo0628@gmail.com>
2023-06-28 00:41:53 +0900

    [ADD] Redshift 접속 인자 추가

---
+++ connect/redshift_session.py
@@ -0,0 +1,16 @@
+import os
+import redshift_connector
+from config.environ import Environ
+
+class RedshiftSession:
+    @classmethod
+    def redshift_conn(cls):
+        conn = redshift_connector.connect(
+            host=Environ.REDSHIFT_HOST,
+            database=Environ.REDSHIFT_DATABASE,
+            port=5439,
+            user=Environ.REDSHIFT_USERNAME,
+            password=Environ.REDSHIFT_PASSWORD
+        )
+        
+        return conn.cursor()
